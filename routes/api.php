fb6525c454da3233b1f78ce9539f2c767d1efab5
adrian <suryawanadriansyah4@gmail.com>
2023-07-26 09:48:42 +0700

    fix error

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\InternshipController;
+use App\Http\Controllers\CompanyAdvisorController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ClassroomController;
-use App\Http\Controllers\SchoolController;
-use App\Http\Controllers\SchoolYearsController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\InternshipRuleController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\StudentController;
-use App\Http\Controllers\StudentDraftController;
+use App\Models\InternshipCompanyJobTitle;
+use App\Models\InternshipCompanyEmployee;
+use App\Models\InternshipCompetency;
+use App\Models\InternshipEquipment;
+use App\Models\InternshipJournal;
+use App\Models\InternshipSuggestion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -37,8 +44,21 @@ Route::group([
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
 });
+
 Route::apiResource('students', StudentController::class);
 Route::apiResource('school-years', SchoolYearsController::class);
 Route::apiResource('classrooms', ClassroomController::class);
 Route::apiResource('schools', SchoolController::class);
 Route::apiResource('student-drafts', StudentDraftController::class);
+Route::apiResource('companies', CompanyController::class);
+Route::get('/internship-rules', [InternshipRuleController::class, 'index']);
+Route::apiResource('notifications', NotificationController::class);
+Route::apiResource('company_advisors', CompanyAdvisorController::class);
+Route::apiResource('internships', InternshipController::class);
+Route::apiResource('internship_company_job_titles', InternshipCompanyJobTitleController::class);
+Route::apiResource('internship_company_employee', InternshipCompanyEmployeeController::class);
+Route::apiResource('internship_journal', InternshipJournalController::class);
+Route::apiResource('internship_journal', InternshipJournalController::class);
+Route::apiResource('internship_suggestion', InternshipSuggestionController::class);
+Route::apiResource('internship_equipment', InternshipEquipmentController::class);
+Route::apiResource('internship_rules', InternshipRuleController::class);
